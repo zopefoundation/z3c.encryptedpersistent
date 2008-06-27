470af42e39a60a4257cc3ff52318183ce95d3905
Stephan Richter <stephan.richter@gmail.com>
2008-06-27 08:36:00 +0000

    * Make all tests pass.
    * Ignore generated files in SVN ops.

--- src/z3c/encryptedpersistent/tests.py
+++ src/z3c/encryptedpersistent/tests.py
@@ -12,10 +12,18 @@ from zope.testing.doctestunit import DocFileSuite
 from zope.app.testing import placelesssetup, setup
 
 
+def setUp(test):
+    placelesssetup.setUp(test)
+    setup.setUpTestAsModule(test, 'README')
+
+def tearDown(test):
+    tearDownTestAsModule(test)
+    placelesssetup.tearDown(test)
+
 def test_suite():
     return unittest.TestSuite((
         DocFileSuite(
             'README.txt',
-            setUp=placelesssetup.setUp, tearDown=placelesssetup.tearDown,
+            setUp=setUp, tearDown=tearDown,
             optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS),
         ))
